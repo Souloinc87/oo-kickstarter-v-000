7ef957524fd3e045ea1d825badacc043d872a48b
Marcus  = Soulo <souloinc87@gmail.com>
2018-04-23 23:10:34 +0000

    automatically backed up by learn

--- lib/backer.rb
+++ lib/backer.rb
@@ -10,7 +10,7 @@ class Backer
   def back_project(project)
     @backed_projects << project
     project = Project.new(project)
-    @backers << @project
+    @backers << project 
 
     binding.pry
 
